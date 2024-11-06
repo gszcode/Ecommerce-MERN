85704f0d5af45ff82069e2a63b37df507014ee79
Gabriel Sanchez <gszcode@gmail.com>
2024-11-05 23:26:00 -0300

    ADD changes

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const router = require("./routes");
 
 const app = express();
-const PORT = 8000;
+const PORT = 8080 || process.env.PORT;
 
 app.use(cors());
+app.use(express.json());
+app.use("/api", router);
 
 connectDB().then(() => {
 	app.listen(PORT, () => {
