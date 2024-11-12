fdaf43b26a55cc91679f57a6ffbd8490f58ae803
Gabriel Sanchez <gszcode@gmail.com>
2024-11-12 01:16:52 -0300

    ADD allusers route and endpoint

---
+++ backend/controllers/allUsers.js
@@ -0,0 +1,22 @@
+const userModel = require("../models/userModel");
+
+async function allUsersCtrl(req, res) {
+	try {
+		const allUsers = await userModel.find();
+
+		res.json({
+			message: "All users",
+			data: allUsers,
+			success: true,
+			error: false,
+		});
+	} catch (error) {
+		res.status(400).json({
+			message: error.message || error,
+			error: true,
+			success: false,
+		});
+	}
+}
+
+module.exports = allUsersCtrl;
