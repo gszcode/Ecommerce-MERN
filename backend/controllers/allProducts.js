acc4698afbb8773b5088e52dc2a303ba53b08084
Gabriel Sanchez <gszcode@gmail.com>
2024-11-15 14:28:06 -0300

    ADD all products endpoint and route

---
+++ backend/controllers/allProducts.js
@@ -0,0 +1,22 @@
+const productModel = require("../models/productModel");
+
+async function allProductsCtrl(req, res) {
+	try {
+		const products = await productModel.find({}).sort({ createdAt: -1 });
+
+		res.status(200).json({
+			message: "All Products",
+			data: products,
+			success: true,
+			error: false,
+		});
+	} catch (error) {
+		res.status(400).json({
+			message: error.message || error,
+			error: true,
+			success: false,
+		});
+	}
+}
+
+module.exports = allProductsCtrl;
