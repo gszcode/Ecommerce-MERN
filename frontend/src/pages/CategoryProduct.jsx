8ada4c38574d48f286ec4490d04fc13b2c747787
Gabriel Sanchez <gszcode@gmail.com>
2024-11-19 23:35:39 -0300

    ADD categoryProduct component

---
+++ frontend/src/pages/CategoryProduct.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom";
+
+function CategoryProduct() {
+	const params = useParams()
+	return (
+		<div>
+			<h1>CategoryProduct</h1>
+		</div>
+	);
+}
+
+export default CategoryProduct;
