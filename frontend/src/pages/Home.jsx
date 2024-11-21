c999d463a5b920a07da5b19f639e34af47a84568
Gabriel Sanchez <gszcode@gmail.com>
2024-11-20 23:12:32 -0300

    ADD banner and products

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,24 @@
 import BannerProduct from "../components/BannerProduct";
 import CategoryList from "../components/CategoryList";
+import HorizontalCardProduct from "../components/HorizontalCardProduct";
+import VerticalCardProduct from "../components/VerticalCardProduct";
 
 const Home = () => {
 	return (
 		<div>
 			<CategoryList />
 			<BannerProduct />
+
+			<HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"} />
+			<HorizontalCardProduct category={"watches"} heading={"Pupolar's Watches"} />
+			<VerticalCardProduct category={"mobile"} heading={"Mobiles"} />
+			<VerticalCardProduct category={"mouse"} heading={"Mouse"} />
+			<VerticalCardProduct category={"tv"} heading={"Televisions"} />
+			<VerticalCardProduct category={"camera"} heading={"Camera & Photografy"} />
+			<VerticalCardProduct category={"earphones"} heading={"Wired Earphones"} />
+			<VerticalCardProduct category={"speakers"} heading={"Bluetooth Speakers"} />
+			<VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"} />
+			<VerticalCardProduct category={"trimmers"} heading={"Trimmers"} />
 		</div>
 	);
 };
