457e180ace8a4de34ae0665077df6d7faba62893
Gabriel Sanchez <gszcode@gmail.com>
2024-11-27 20:13:09 -0300

    ADD cart functionality

--- frontend/src/utils/addToCart.js
+++ frontend/src/utils/addToCart.js
@@ -1,4 +1,22 @@
-export function addToCart(e, id) {
+import { SummaryApi } from "../common";
+import { toast } from "react-toastify";
+
+export async function addToCart(e, id) {
 	e.stopPropagation();
-	e.preventDefault()
+	e.preventDefault();
+
+	const dataResponse = await fetch(SummaryApi.addToCart.url, {
+		method: SummaryApi.addToCart.method,
+		credentials: "include",
+		headers: {
+			"Content-type": "application/json",
+		},
+		body: JSON.stringify({ productId: id }),
+	});
+
+	const dataApi = await dataResponse.json();
+	if (dataApi.success) toast.success(dataApi.message);
+	if (dataApi.error) toast.error(dataApi.message);
+
+	return dataApi;
 }
