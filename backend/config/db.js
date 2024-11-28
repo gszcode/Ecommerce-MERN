9a33f6fda0c2a970e7897c720757876d95e7329e
Gabriel Sanchez <gszcode@gmail.com>
2024-11-28 02:51:42 -0300

    FIX bug

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,14 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
 	try {
-		await mongoose.connect(process.env.MONGODB_URI);
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
-		console.log(error);
+		console.error(`Error: ${error.message}`);
+		process.exit(1); // Detener el proceso si falla la conexión
 	}
 }
 
