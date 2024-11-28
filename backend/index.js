dd5d6b2ceace16bc37fcc7c518420e4435442193
Gabriel Sanchez <gszcode@gmail.com>
2024-11-28 02:44:48 -0300

    FIX bug

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const router = require("./routes");
 
 const app = express();
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(
 	cors({
